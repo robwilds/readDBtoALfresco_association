ac263df75f7ec967f568321f39f5b57e4680c0ac
rob wilds <robwilds@me.com>
2023-08-02 18:04:20 -0400

    updated 7.3 folders

--- readXLS.py
+++ readXLS.py
@@ -13,8 +13,8 @@ hiddenFolder = '37d3c179-cf42-4f67-99ea-22bcabf763d7'
 
 #globals for 7.3 install
 """ globalBaseURL = 'http://ec2-3-90-226-222.compute-1.amazonaws.com' #for 7.3 EC2
-baseFolder = '22515311-84c6-48ba-971f-8ea08cd56628' #for 7.4 EC2 - Associations folder
-hiddenFolder = '37d3c179-cf42-4f67-99ea-22bcabf763d7' """
+baseFolder = '2fdca4f6-f926-4819-b42e-b8fc993314e6' #for 7.4 EC2 - Associations folder
+hiddenFolder = 'e9ca1a5b-844a-4836-a55e-86eba511293c' """
 
 fileName = '' #'doctornote.jpg'  #for testing
 targetNode = ''
