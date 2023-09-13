da32130016894c5c9a68f445e428f69f72cbd5a2
Rob Wilds <robwilds@me.com>
2023-09-13 08:11:00 -0400

    updated update body to include description

--- updateNode.py
+++ updateNode.py
@@ -4,8 +4,8 @@ def updateNode(targetNodeID,alfURL,caseID,plaintiff,defendent,docID,DocFileName,
     _url = alfURL + "/alfresco/api/-default-/public/alfresco/versions/1/nodes/" + targetNodeID
 
     #data = "{\"properties\":{\"my:caseID\":\""+ caseID+ "\"}}"    ,\"my:plaintiff\":\""+ plaintiff+ "\"
-    data = "{\"properties\":{\"my:caseID\":\""+ caseID+ "\",\"my:plaintiff\":\""+ plaintiff+ "\",\"my:defendant\":\""+ defendent+ "\",\"my:DocID\":\""+ str(docID)+ "\",\"my:DocTitle\":\""+ Doctitle+ "\",\"my:Author\":\""+ Author+ "\",\"my:Recipient\":\""+ Recipient+ "\",\"my:RecipientTitle\":\""+ Recipienttitle+ "\",\"my:RecipientAgency\":\""+ RecipientAgency+ "\",\"my:DocType\":\""+ DocType+ "\",\"my:FOIANotes\":\""+ foiaNotes+ "\"}}"  
-
+    # original -> data = "{\"properties\":{\"my:caseID\":\""+ caseID+ "\",\"my:plaintiff\":\""+ plaintiff+ "\",\"my:defendant\":\""+ defendent+ "\",\"my:DocID\":\""+ str(docID)+ "\",\"my:DocTitle\":\""+ Doctitle+ "\",\"my:Author\":\""+ Author+ "\",\"my:Recipient\":\""+ Recipient+ "\",\"my:RecipientTitle\":\""+ Recipienttitle+ "\",\"my:RecipientAgency\":\""+ RecipientAgency+ "\",\"my:DocType\":\""+ DocType+ "\",\"my:FOIANotes\":\""+ foiaNotes+ "\"}}"  
+    data = "{\"properties\":{\"cm:description\":\"author->" + Author+" doctype-> "+ DocType +"\",\"my:caseID\":\""+ caseID+ "\",\"my:plaintiff\":\""+ plaintiff+ "\",\"my:defendant\":\""+ defendent+ "\",\"my:DocID\":\""+ str(docID)+ "\",\"my:DocTitle\":\""+ Doctitle+ "\",\"my:Author\":\""+ Author+ "\",\"my:Recipient\":\""+ Recipient+ "\",\"my:RecipientTitle\":\""+ Recipienttitle+ "\",\"my:RecipientAgency\":\""+ RecipientAgency+ "\",\"my:DocType\":\""+ DocType+ "\",\"my:FOIANotes\":\""+ foiaNotes+ "\"}}"
     print("update data is: " + data +" for nodeid: "+targetNodeID)
 
     print (requests.put(_url,data,auth=('demo','demo')))
